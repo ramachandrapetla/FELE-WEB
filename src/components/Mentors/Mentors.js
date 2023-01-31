9d86cc4a11119b29262a9bc1605ef5fd6f9c3355
SoorajSNBlaze333 <soorajsnblaze333@gmail.com>
2023-01-30 21:08:59 -0600

    Updated Mentors UI

--- src/components/Mentors/Mentors.js
+++ src/components/Mentors/Mentors.js
@@ -1,11 +1,12 @@
-import { EnvelopeSimple } from 'phosphor-react';
 import Default from '../../assets/default.png';
+import { EnvelopeSimple } from 'phosphor-react';
 
 export default function Mentors() {
   return (
     <div className='body-height p-8 grid grid-cols-2 grid-rows-1 gap-8'>
       <div className='col-span-1 flex justify-center items-center flex-col'>
-        <img className='rounded-full' src={Default} alt="f"/>
+        <div className='h-[300px] w-[300px] bg-yellow-200 mb-4 rounded-full'></div>
+        {/* <img className='rounded-full' src={Default} alt="f"/> */}
         <p className="m-0 mb-2 text-xl font-semibold">Dr. Kwok-Bun Yue</p>
         <p className='m-0 text-center w-[250px] mb-2'>Professor of Computer Science and Computer Information Systems, College of Science and Engineering</p>
         <div className="flex justify-center items-center mb-2">
@@ -15,7 +16,8 @@ export default function Mentors() {
         <p className=''>Office: Delta 163</p>
       </div>
       <div className='col-span-1 flex justify-center items-center flex-col'>
-        <img className='rounded-full' src={Default} alt="f"/>
+        <div className='h-[300px] w-[300px] bg-emerald-200 mb-4 rounded-full'></div>
+        {/* <img className='rounded-full' src={Default} alt="f"/> */}
         <p className="m-0 mb-2 text-xl font-semibold">Dr. Kewei Sha</p>
         <p className='m-0 text-center w-[250px] mb-2'>Assistant Professor of Computer Science, College of Science and Engineering</p>
         <div className="flex justify-center items-center mb-2">
